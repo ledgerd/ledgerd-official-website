822d4dc9c5636a0390a187534477644abfd781cc
Lord <yongzifei@gmail.com>
2018-03-21 16:19:23 +0800

    add pages

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,24 @@ exports = module.exports = function (app) {
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 	app.all('/contact', routes.views.contact);
+	app.get('/member', routes.views.member);
+	app.get('/download-wallet', routes.views.wallet_download);
+	app.get('/get-lgd', routes.views.get_lgd);
+	app.get('/validator', routes.views.validator);
+	app.get('/market-maker', routes.views.market_maker);
+	app.get('/gateway', routes.views.gateway);
+	app.get('/org', routes.views.org);
+	app.get('/project', routes.views.project);
+	app.get('/govern', routes.views.govern);
+	app.get('/proposal', routes.views.proposal);
+	app.get('/jobs', routes.views.jobs);
+	app.get('/vote', routes.views.vote);
+	app.get('/vote/history', routes.views.vote_history);
+	app.get('/docs', routes.views.docs);
+	app.get('/tools', routes.views.tools);
+	app.get('/policy', routes.views.policy);
+	app.get('/terms', routes.views.terms);
+	app.get('/election', routes.views.election);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
